ae2d167914f3d341dc7107262b34d94f6d71c72c
Rijo George <rijogeorge@rijos-imac.lan>
2021-06-01 11:11:46 -0700

    instruction 3 done

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 const qwerty = document.getElementById("qwerty");
 const phrase = document.getElementById("phrase");
 let missed = 0;
+const resetButton = document.querySelector(".btn__reset");
+resetButton.addEventListener("click", (event) => {
+    console.log("button clicked");
+});
