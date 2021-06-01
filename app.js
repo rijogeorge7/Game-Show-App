5125039afeddc444746562e8fc33318f7bfa5ee9
Rijo George <rijogeorge@rijos-imac.lan>
2021-06-01 11:23:32 -0700

    instruction 5 done

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const phrase = document.getElementById("phrase");
 let missed = 0;
 const resetButton = document.querySelector(".btn__reset");
 const phrases = ["like you","love you","prolong","life is not easy","life is wonderful"];
+
 resetButton.addEventListener("click", (event) => {
-    console.log(phrases);
+    console.log(getRandomPhraseAsArray(phrases));
 });
+
+function getRandomPhraseAsArray(arr){
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    const randomString = arr[randomIndex];
+    return Array.from(randomString);
+} 
