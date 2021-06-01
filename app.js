a38d5b0eafe9af2907dff0dc348cab3813333869
Rijo George <rijogeorge@rijos-imac.lan>
2021-06-01 11:16:10 -0700

    instruction 4 done

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const qwerty = document.getElementById("qwerty");
 const phrase = document.getElementById("phrase");
 let missed = 0;
 const resetButton = document.querySelector(".btn__reset");
+const phrases = ["like you","love you","prolong","life is not easy","life is wonderful"];
 resetButton.addEventListener("click", (event) => {
-    console.log("button clicked");
+    console.log(phrases);
 });
